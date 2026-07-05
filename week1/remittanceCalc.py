470356fd7c85e2504d9c2a5483283ad62db446cb
prashanna dhami <prashannadhami14@gmail.com>
2026-07-05 12:14:18 +0545

    change USD to quatar riyal

--- week1/remittanceCalc.py
+++ week1/remittanceCalc.py
@@ -1,14 +1,14 @@
 # Remittance Calculator
-usd_amount = float(input("Enter amount in USD: "))
-exchange_rate = float(input("Enter current exchange rate (USD to NPR): "))
+qatar_amount = float(input("Enter amount in qatar: "))
+exchange_rate = float(input("Enter current exchange rate (qatar to NPR): "))
 fee_percentage = float(input("Enter service fee percentage: "))
 
-fee = usd_amount * (fee_percentage / 100)
-amount_after_fee = usd_amount - fee
+fee = qatar_amount * (fee_percentage / 100)
+amount_after_fee = qatar_amount - fee
 npr_amount = amount_after_fee * exchange_rate
 
 print(f"\n--- Remittance Summary ---")
-print(f"Amount sent: ${usd_amount:.2f}")
-print(f"Fee charged: ${fee:.2f}")
-print(f"Amount after fee: ${amount_after_fee:.2f}")
+print(f"Amount sent: {qatar_amount:.2f}")
+print(f"Fee charged: {fee:.2f}")
+print(f"Amount after fee: {amount_after_fee:.2f}")
 print(f"Amount received in NPR: Rs. {npr_amount:.2f}")
